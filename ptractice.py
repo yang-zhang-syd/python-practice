e4fc9f5eff9a2ca459a77d73859991b124e0a711
Yang Zhang <yang.zhang@icloud.com>
2022-05-24 21:26:57 +1000

    2023-05-24

--- ptractice.py
+++ ptractice.py
@@ -1,33 +1,51 @@
+from typing import List
+import collections
+
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # setup connections between words
+        dic = {}
+        nodesDic = {}
+        nodes = map(lambda x: [x, False, 0, False], wordList)
+        for node in nodes:
+            nodesDic[node[0]] = node
         
-        max = (0, 0)
-        prev = {}
-        for n in range(len(s)):
-            if n % 2 == 0:
-                p = {}
-            for i in range(len(s)):
-                j = i + n
-                if j >= len(s):
-                    break
-                k = i + 1
-                l = j - 1
-                if k >= l:
-                    p[str(i)+','+str(j)] = s[i] == s[j]
-
+        for word in wordList:
+            for i in range(len(word)):
+                key = word[:i] + '*' + word[i+1:]
+                if key in dic:
+                    dic[key].append(nodesDic[word]) # word and visited flag as dictionary value
                 else:
-                    p[str(i)+','+str(j)] = prev[str(k)+','+str(l)] and s[i] == s[j]
-                    
-                if p[str(i)+','+str(j)]:
-                    length = j - i + 1
-                    if length > max[1] - max[0] + 1:
-                        max = (i, j)
-            if n % 2 == 1:
-                prev = p
+                    dic[key] = [nodesDic[word]]
+                
+        # search for end word
+        count = 0
+        current = beginWord
+        connected = self.findConnectedWords(current, dic, 1)
+        while connected:
+            first = connected.pop(0)
+            first[1] = True
+            l = first[2]
+            if first[0] == endWord:
+                return first[2] + 1
+            else:
+                connected1 = self.findConnectedWords(first[0], dic, l + 1)
+                connected += connected1
 
-        return s[max[0]:max[1]+1]
+        return 0
 
+    def findConnectedWords(self, word, dic, level):
+        candidates = []
+        for i in range(len(word)):
+            key = word[:i] + '*' + word[i+1:]
+            if key in dic:
+                for candidate in dic[key]:
+                    if not candidate[1] and candidate[0] != word and not candidate[3]:
+                        candidate[2] = level
+                        candidate[3] = True
+                        candidates.append(candidate)
+        return candidates
 
 s = Solution()
-r = s.longestPalindrome("abcba")
+r = s.ladderLength('hit', 'cog', ["hot","dot","dog","lot","log","cog"])
 print(r)
