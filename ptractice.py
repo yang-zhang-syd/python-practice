7b5fe15d2de20c593d8cb4b85454d5e981d5a360
Yang Zhang <yang.zhang@icloud.com>
2022-05-26 20:36:25 +1000

    2022-05-26

--- ptractice.py
+++ ptractice.py
@@ -6,7 +6,7 @@ class Solution:
         # setup connections between words
         dic = {}
         nodesDic = {}
-        nodes = map(lambda x: [x, False, 0, False], wordList)
+        nodes = map(lambda x: [x, False, 0, False, None, []], wordList)
         for node in nodes:
             nodesDic[node[0]] = node
         
@@ -20,29 +20,34 @@ class Solution:
                 
         # search for end word
         count = 0
-        current = beginWord
-        connected = self.findConnectedWords(current, dic, 1)
+        connected = self.findConnectedWords([beginWord, False, 0, False, None, []], dic, 1, endWord)
+        paths = []
         while connected:
             first = connected.pop(0)
             first[1] = True
-            l = first[2]
+            level = first[2]
+            if len(paths) > 0 and len(paths[0]) < level:
+                break
             if first[0] == endWord:
-                return first[2] + 1
-            else:
-                connected1 = self.findConnectedWords(first[0], dic, l + 1)
-                connected += connected1
+                paths.append(first[5] + [first[0]])
 
-        return 0
+            connected1 = self.findConnectedWords(first, dic, level + 1, endWord)
+            connected += connected1
 
-    def findConnectedWords(self, word, dic, level):
+        return paths
+
+    def findConnectedWords(self, node, dic, level, endWord):
+        word = node[0]
         candidates = []
         for i in range(len(word)):
             key = word[:i] + '*' + word[i+1:]
             if key in dic:
                 for candidate in dic[key]:
-                    if not candidate[1] and candidate[0] != word and not candidate[3]:
+                    if not candidate[1] and candidate[0] != word and (not candidate[3] or candidate[0] == endWord):
                         candidate[2] = level
                         candidate[3] = True
+                        candidate[5] = node[5] + [word]
+                        candidate[4] = node
                         candidates.append(candidate)
         return candidates
 
