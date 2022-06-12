840af0d2d5a486e88c5f3748fd155be20742b312
Yang Zhang <yang.zhang@icloud.com>
2022-06-13 07:39:22 +1000

    2022-06-13

--- 113.PathSumII.py
+++ 113.PathSumII.py
@@ -1,10 +1,50 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        return []
+        
+        if not root:
+            return []
+
+        if not root.left and not root.right:
+            if root.val == targetSum:
+                return [[root.val]]
+            else:
+                return []
+
+        leftLists = []
+        if root.left:
+            leftLists = self.pathSum_rec(root.left, root.val, targetSum, [root.val])
+        
+        rightLists = []
+        if root.right:
+            rightLists = self.pathSum_rec(root.right, root.val, targetSum, [root.val])
+
+        return leftLists + rightLists
+        
+        
+    def pathSum_rec(self, currentNode: Optional[TreeNode], prevSum: int, targetSum: int, path: List[int]) -> List[List[int]]:
+        
+        prevSum += currentNode.val
+
+        if not currentNode.left and not currentNode.right:
+            if prevSum == targetSum:
+                path.append(currentNode.val)
+                return [path]
+            else:
+                return []
+        
+        newPathLeft = list(path)
+        newPathLeft.append(currentNode.val)
+
+        leftLists = []
+        if currentNode.left:
+            leftLists = self.pathSum_rec(currentNode.left, prevSum, targetSum, newPathLeft)
+
+        newPathRight = list(path)
+        newPathRight.append(currentNode.val)
+
+        rightLists = []
+        if currentNode.right:
+            rightLists = self.pathSum_rec(currentNode.right, prevSum, targetSum, newPathRight)
+
+        return leftLists + rightLists
+        
